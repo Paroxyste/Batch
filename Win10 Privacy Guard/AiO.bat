b2cd77f2b1bd4d121a887d33c1684eca919b05df
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2020-06-03 15:48:40 +0200

    Fix service + specific services block

--- Win10 Privacy Guard/AiO.bat
+++ Win10 Privacy Guard/AiO.bat
@@ -1,376 +1,362 @@
 powershell Get-AppxPackage -AllUsers | Remove-AppxPackage
 
-net stop AJRouter
+sc stop AJRouter
 sc config AJRouter start= disabled
-sc delete AJRouter
+sc delete AJRouter
 
-net stop ALG
+sc stop ALG
 sc config ALG start= disabled
-sc delete ALG
+sc delete ALG
 
-net stop AppVClient
+sc stop AppVClient
 sc config AppVClient start= disabled
-sc delete AppVClient
+sc delete AppVClient
 
-net stop AxInstSV
+sc stop AxInstSV
 sc config AxInstSV start= disabled
-sc delete AxInstSV
+sc delete AxInstSV
 
-net stop BDESVC
-sc config BDESVC start= disabled
-sc delete BDESVC
-
-net stop BTAGService
+sc stop BTAGService
 sc config BTAGService start= disabled
-sc delete BTAGService
+sc delete BTAGService
 
-net stop BthAvctpSvc
+sc stop BthAvctpSvc
 sc config BthAvctpSvc start= disabled
-sc delete BthAvctpSvc
+sc delete BthAvctpSvc
 
-net stop bthserv
+sc stop bthserv
 sc config bthserv start= disabled
-sc delete bthserv
+sc delete bthserv
 
-net stop CDPSvc
+sc stop CDPSvc
 sc config CDPSvc start= disabled
-sc delete CDPSvc
+sc delete CDPSvc
 
-net stop CertPropSvc
-sc config CertPropSvc start= disabled
-sc delete CertPropSvc
-
-net stop CscService
-sc config CscService start= disabled
-sc delete CscService
-
-net stop diagnosticshub.standardcollector.service
+sc stop diagnosticshub.standardcollector.service
 sc config diagnosticshub.standardcollector.service start= disabled
-sc delete diagnosticshub.standardcollector.service
+sc delete diagnosticshub.standardcollector.service
 
-net stop diagtrack
+sc stop diagtrack
 sc config diagtrack start= disabled
-sc delete diagtrack
+sc delete diagtrack
 
-net stop dmwappushservice
+sc stop dmwappushservice
 sc config dmwappushservice start= disabled
-sc delete dmwappushservice
+sc delete dmwappushservice
 
-net stop 
-sc config DPS start= disabled
-sc delete DPS
-
-net stop Fax
+sc stop Fax
 sc config Fax start= disabled
-sc delete Fax
+sc delete Fax
 
-net stop fdPHost
+sc stop fdPHost
 sc config fdPHost start= disabled
-sc delete fdPHost
+sc delete fdPHost
 
-net stop FDResPub
+sc stop FDResPub
 sc config FDResPub start= disabled
-sc delete FDResPub
+sc delete FDResPub
 
-net stop fhsvc
+sc stop fhsvc
 sc config fhsvc start= disabled
-sc delete fhsvc
+sc delete fhsvc
 
-net stop icssvc
+sc stop icssvc
 sc config icssvc start= disabled
-sc delete icssvc
+sc delete icssvc
 
-net stop IKEEXT
+sc stop IKEEXT
 sc config IKEEXT start= disabled
-sc delete IKEEXT
+sc delete IKEEXT
 
-net stop iphlpsvc
-sc config iphlpsvc start= disabled
-sc delete iphlpsvc
-
-net stop lfsvc
+sc stop lfsvc
 sc config lfsvc start= disabled
-sc delete lfsvc
+sc delete lfsvc
 
-net stop lltdsvc
+sc stop lltdsvc
 sc config lltdsvc start= disabled
-sc delete lltdsvc
+sc delete lltdsvc
 
-net stop lmhosts
+sc stop lmhosts
 sc config lmhosts start= disabled
-sc delete lmhosts
+sc delete lmhosts
 
-net stop MapsBroker
+sc stop MapsBroker
 sc config MapsBroker start= disabled
-sc delete MapsBroker
+sc delete MapsBroker
 
-net stop MSiSCSI
+sc stop MSiSCSI
 sc config MSiSCSI start= disabled
-sc delete MSiSCSI
+sc delete MSiSCSI
 
-net stop NcaSvc
+sc stop NcaSvc
 sc config NcaSvc start= disabled
-sc delete NcaSvc
+sc delete NcaSvc
 
-net stop NcbService
+sc stop NcbService
 sc config NcbService start= disabled
-sc delete NcbService
+sc delete NcbService
 
-net stop Netlogon
+sc stop Netlogon
 sc config Netlogon start= disabled
-sc delete Netlogon
+sc delete Netlogon
 
-net stop NetTcpPortSharing
+sc stop NetTcpPortSharing
 sc config NetTcpPortSharing start= disabled
-sc delete NetTcpPortSharing
+sc delete NetTcpPortSharing
 
-net stop p2pimsvc
+sc stop p2pimsvc
 sc config p2pimsvc start= disabled
-sc delete p2pimsvc
+sc delete p2pimsvc
 
-net stop PcaSvc
+sc stop PcaSvc
 sc config PcaSvc start= disabled
-sc delete PcaSvc
+sc delete PcaSvc
 
-net stop PeerDistSvc
+sc stop PeerDistSvc
 sc config PeerDistSvc start= disabled
-sc delete PeerDistSvc
+sc delete PeerDistSvc
 
-net stop PhoneSvc
+sc stop PhoneSvc
 sc config PhoneSvc start= disabled
-sc delete PhoneSvc
+sc delete PhoneSvc
 
-net stop PNRPAutoReg
+sc stop PNRPAutoReg
 sc config PNRPAutoReg start= disabled
-sc delete PNRPAutoReg
+sc delete PNRPAutoReg
 
-net stop PNRPsvc
+sc stop PNRPsvc
 sc config PNRPsvc start= disabled
-sc delete PNRPsvc
+sc delete PNRPsvc
 
-net stop RasAuto
+sc stop RasAuto
 sc config RasAuto start= disabled
-sc delete RasAuto
+sc delete RasAuto
 
-net stop RasMan
+sc stop RasMan
 sc config RasMan start= disabled
-sc delete RasMan
+sc delete RasMan
 
-net stop RemoteAccess
+sc stop RemoteAccess
 sc config RemoteAccess start= disabled
-sc delete RemoteAccess
+sc delete RemoteAccess
 
-net stop RemoteRegistry
+sc stop RemoteRegistry
 sc config RemoteRegistry start= disabled
-sc delete RemoteRegistry
+sc delete RemoteRegistry
 
-net stop RetailDemo
+sc stop RetailDemo
 sc config RetailDemo start= disabled
-sc delete RetailDemo
+sc delete RetailDemo
 
-net stop RpcLocator
+sc stop RpcLocator
 sc config RpcLocator start= disabled
-sc delete RpcLocator
+sc delete RpcLocator
 
-net stop SCardSvr
-sc config SCardSvr start= disabled
-sc delete SCardSvr
-
-net stop 
-sc config ScDeviceEnum start= disabled
-sc delete ScDeviceEnum
-
-net stop SCPolicySvc
-sc config SCPolicySvc start= disabled
-sc delete SCPolicySvc
-
-net stop 
+sc stop seclogon
 sc config seclogon start= disabled
-sc delete seclogon
+sc delete seclogon
 
-net stop SENS
+sc stop SENS
 sc config SENS start= disabled
-sc delete SENS
+sc delete SENS
 
-net stop SensrSvc
+sc stop SensrSvc
 sc config SensrSvc start= disabled
-sc delete SensrSvc
+sc delete SensrSvc
 
-net stop SessionEnv
+sc stop SessionEnv
 sc config SessionEnv start= disabled
-sc delete SessionEnv
+sc delete SessionEnv
 
-net stop SharedAccess
+sc stop SharedAccess
 sc config SharedAccess start= disabled
-sc delete SharedAccess
+sc delete SharedAccess
 
-net stop shpamsvc
+sc stop shpamsvc
 sc config shpamsvc start= disabled
-sc delete shpamsvc
+sc delete shpamsvc
 
-net stop smphost
+sc stop smphost
 sc config smphost start= disabled
-sc delete smphost
+sc delete smphost
 
-net stop SmsRouter
-sc config SmsRouter start= disabled
-sc delete SmsRouter
-
-net stop SNMPTRAP
+sc stop SNMPTRAP
 sc config SNMPTRAP start= disabled
-sc delete SNMPTRAP
+sc delete SNMPTRAP
 
-net stop Spooler
+sc stop Spooler
 sc config Spooler start= disabled
-sc delete Spooler
+sc delete Spooler
 
-net stop ssh-agent
+sc stop ssh-agent
 sc config ssh-agent start= disabled
-sc delete ssh-agent
+sc delete ssh-agent
 
-net stop stisvc
+sc stop stisvc
 sc config stisvc start= disabled
-sc delete stisvc
+sc delete stisvc
 
-net stop StorSvc
+sc stop StorSvc
 sc config StorSvc start= disabled
-sc delete StorSvc
+sc delete StorSvc
 
-net stop TabletInputService
+sc stop TabletInputService
 sc config TabletInputService start= disabled
-sc delete TabletInputService
+sc delete TabletInputService
 
-net stop TapiSrv
+sc stop TapiSrv
 sc config TapiSrv start= disabled
-sc delete TapiSrv
+sc delete TapiSrv
 
-net stop TermService
-sc config TermService start= disabled
-sc delete TermService
-
-net stop TokenBroker
+sc stop TokenBroker
 sc config TokenBroker start= disabled
-sc delete TokenBroker
+sc delete TokenBroker
 
-net stop TrkWks
+sc stop TrkWks
 sc config TrkWks start= disabled
-sc delete TrkWks
+sc delete TrkWks
 
-net stop UevAgentService
+sc stop UevAgentService
 sc config UevAgentService start= disabled
-sc delete UevAgentService
+sc delete UevAgentService
 
-net stop UmRdpService
+sc stop UmRdpService
 sc config UmRdpService start= disabled
-sc delete UmRdpService
+sc delete UmRdpService
 
-net stop VaultSvc
+sc stop VaultSvc
 sc config VaultSvc start= disabled
-sc delete VaultSvc
+sc delete VaultSvc
 
-net stop vmicguestinterface
+sc stop vmicguestinterface
 sc config vmicguestinterface start= disabled
-sc delete vmicguestinterface
+sc delete vmicguestinterface
 
-net stop vmickvpexchange
+sc stop vmickvpexchange
 sc config vmickvpexchange start= disabled
-sc delete vmickvpexchange
+sc delete vmickvpexchange
 
-net stop vmicrdv
+sc stop vmicrdv
 sc config vmicrdv start= disabled
-sc delete vmicrdv
+sc delete vmicrdv
 
-net stop vmicshutdown
+sc stop vmicshutdown
 sc config vmicshutdown start= disabled
-sc delete vmicshutdown
+sc delete vmicshutdown
 
-net stop vmictimesync
+sc stop vmictimesync
 sc config vmictimesync start= disabled
-sc delete vmictimesync
+sc delete vmictimesync
 
-net stop vmicvmsession
+sc stop vmicvmsession
 sc config vmicvmsession start= disabled
-sc delete vmicvmsession
+sc delete vmicvmsession
 
-net stop vmicvss
+sc stop vmicvss
 sc config vmicvss start= disabled
-sc delete vmicvss
+sc delete vmicvss
 
-net stop WbioSrvc
+sc stop WbioSrvc
 sc config WbioSrvc start= disabled
-sc delete WbioSrvc
+sc delete WbioSrvc
 
-net stop wcncsvc
+sc stop wcncsvc
 sc config wcncsvc start= disabled
-sc delete wcncsvc
+sc delete wcncsvc
 
-net stop WdiServiceHost
-sc config WdiServiceHost start= disabled
-sc delete WdiServiceHost
-
-net stop WdiSystemHost
-sc config WdiSystemHost start= disabled
-sc delete WdiSystemHost
-
-net stop WebClient
+sc stop WebClient
 sc config WebClient start= disabled
-sc delete WebClient
+sc delete WebClient
 
-net stop Wecsvc
+sc stop Wecsvc
 sc config Wecsvc start= disabled
-sc delete Wecsvc
+sc delete Wecsvc
 
-net stop wercplsupport
+sc stop wercplsupport
 sc config wercplsupport start= disabled
-sc delete wercplsupport
+sc delete wercplsupport
 
-net stop WerSvc
+sc stop WerSvc
 sc config <service_name> start= disabled
-sc delete WerSvc
+sc delete WerSvc
 
-net stop WinRM
+sc stop WinRM
 sc config WinRM start= disabled
-sc delete WinRM
+sc delete WinRM
 
-net stop wisvc
+sc stop wisvc
 sc config wisvc start= disabled
-sc delete wisvc
+sc delete wisvc
 
-net stop wlidsvc
+sc stop wlidsvc
 sc config wlidsvc start= disabled
-sc delete wlidsvc
+sc delete wlidsvc
 
-net stop WpcMonSvc
+sc stop WpcMonSvc
 sc config WpcMonSvc start= disabled
-sc delete WpcMonSvc
+sc delete WpcMonSvc
 
-net stop WPDBusEnum
+sc stop WPDBusEnum
 sc config WPDBusEnum start= disabled
-sc delete WPDBusEnum
+sc delete WPDBusEnum
 
-net stop WpnService
+sc stop WpnService
 sc config WpnService start= disabled
-sc delete WpnService
+sc delete WpnService
 
-net stop WSearch
-sc config WSearch start= disabled
-sc delete WSearch
-
-net stop WwanSvc
-sc config WwanSvc start= disabled
-sc delete WwanSvc
-
-net stop XblAuthManager
+sc stop XblAuthManager
 sc config XblAuthManager start= disabled
-sc delete XblAuthManager
+sc delete XblAuthManager
 
-net stop XblGameSave
+sc stop XblGameSave
 sc config XblGameSave start= disabled
-sc delete XblGameSave
+sc delete XblGameSave
 
-net stop XboxNetApiSvc
+sc stop XboxNetApiSvc
 sc config XboxNetApiSvc start= disabled
-sc delete XboxNetApiSvc
+sc delete XboxNetApiSvc
+
+sc stop BDESVC
+sc config BDESVC start= disabled
+
+sc stop CertPropSvc
+sc config CertPropSvc start= disabled
+
+sc stop CscService
+sc config CscService start= disabled
+
+sc stop DPS
+sc config DPS start= disabled
+
+sc stop iphlpsvc
+sc config iphlpsvc start= disabled
+
+sc stop SCardSvr
+sc config SCardSvr start= disabled
+
+sc stop ScDeviceEnum
+sc config ScDeviceEnum start= disabled
+
+sc stop SCPolicySvc
+sc config SCPolicySvc start= disabled
+
+sc stop SmsRouter
+sc config SmsRouter start= disabled
+
+sc stop TermService
+sc config TermService start= disabled
+
+sc stop WwanSvc
+sc config WwanSvc start= disabled
+
+sc stop WdiServiceHost
+sc config WdiServiceHost start= disabled
+
+sc stop WdiSystemHost
+sc config WdiSystemHost start= disabled
+
+sc stop WSearch
+sc config WSearch start= disabled
 
 schtasks /CHANGE /TN "Microsoft\Office\OfficeTelemetry\AgentFallBack2016" /DISABLE
 schtasks /DELETE /TN "Microsoft\Office\OfficeTelemetry\AgentFallBack2016" /f
@@ -844,6 +830,3 @@ IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^0.0.0.0 wes.df.telemetry.microsoft.com>>%WI
 
 FIND /C /I "www.msftncsi.com" %WINDIR%\system32\drivers\etc\hosts
 IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^0.0.0.0 www.msftncsi.com>>%WINDIR%\System32\drivers\etc\hosts
-
-shutdown /r /t 00
-exit
