afe675cda91bf22572b0aa5221fba75002f94299
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2020-06-03 15:49:22 +0200

    Fix services & Add specific services

---
+++ Win10 Privacy Guard/specific-services.msc.bat
@@ -0,0 +1,41 @@
+sc stop BDESVC
+sc config BDESVC start= disabled
+
+sc stop CertPropSvc
+sc config CertPropSvc start= disabled
+
+sc stop CscService
+sc config CscService start= disabled
+
+sc stop DPS
+sc config DPS start= disabled
+
+sc stop iphlpsvc
+sc config iphlpsvc start= disabled
+
+sc stop SCardSvr
+sc config SCardSvr start= disabled
+
+sc stop ScDeviceEnum
+sc config ScDeviceEnum start= disabled
+
+sc stop SCPolicySvc
+sc config SCPolicySvc start= disabled
+
+sc stop SmsRouter
+sc config SmsRouter start= disabled
+
+sc stop TermService
+sc config TermService start= disabled
+
+sc stop WwanSvc
+sc config WwanSvc start= disabled
+
+sc stop WdiServiceHost
+sc config WdiServiceHost start= disabled
+
+sc stop WdiSystemHost
+sc config WdiSystemHost start= disabled
+
+sc stop WSearch
+sc config WSearch start= disabled
